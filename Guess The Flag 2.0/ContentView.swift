c4787878eec9db94791090c04f9283495952f216
Santhosh2141 <snthshsrinivas@gmail.com>
2025-05-27 20:25:27 +0530

    stacks intro

--- Guess The Flag 2.0/ContentView.swift
+++ Guess The Flag 2.0/ContentView.swift
@@ -9,11 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+        VStack(alignment: .leading, spacing: 20) {
+            Spacer()
+            Spacer()
             Text("Hello, world!")
+            Text("This is another text")
+            HStack(alignment: .bottom, spacing: 10){
+                Text("Hello, world!")
+                Text("This is another text")
+                Text("This is the third text")
+            }
+            HStack(alignment: .bottom, spacing: 10){
+                Text("Hello, world!")
+                Text("This is another text")
+                Text("This is the third text")
+            }
+            HStack(alignment: .bottom, spacing: 10){
+                Text("Hello, world!")
+                Text("This is another text")
+                Text("This is the third text")
+            }
+            ZStack(alignment: .top){
+                Text("Hello, world!")
+                Text("This is another text")
+            }
+            Spacer()
+            //Spacer takew all the remaining spaces
         }
         .padding()
     }
