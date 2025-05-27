ec455fb2fea3e3495eeec630e0f0b8cb8ca88eb4
Santhosh2141 <snthshsrinivas@gmail.com>
2025-05-27 20:09:34 +0530

    Guess the flag project

---
+++ Guess The Flag 2.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Guess The Flag 2.0
+//
+//  Created by Santhosh Srinivas on 27/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess The Flag 2.0/Guess_The_Flag_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_Flag_2_0App.swift
+//  Guess The Flag 2.0
+//
+//  Created by Santhosh Srinivas on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_Flag_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
